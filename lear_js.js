6aa1f76d620fee2d8c92202f753b370bc11b59d7
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2025-07-30 15:17:20 +0300

    Update lear_js.js

--- lear_js.js
+++ lear_js.js
@@ -13,5 +13,19 @@ console.log(counter1()); // 2
 console.log(counter1()); // 3
 
 const counter2 = makeCounter();
-console.log(counter2()); // 1 — новий лічильник!  
+console.log(counter2()); // 1 — новий лічильник
+
+function makeDecrementer() {
+    let count = 10;
+    return function() {
+        if (count > 0) {
+            count--;
+            return count;
+        }
+    }
+}
+const decrementer = makeDecrementer();
+console.log(decrementer()); // 9
+console.log(decrementer()); // 8    
+console.log(decrementer()); // 7
 */
